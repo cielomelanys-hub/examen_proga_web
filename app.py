11983cd409a6daf0d64cab03de4aa8e744c34b09
cielomelanys-hub <cielomelanys@gmail.com>
2026-03-24 09:43:01 -0400

    nuevocambio
    cambiooo

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import sqlite3
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -85,6 +86,7 @@ def index():
     return render_template('index.html', restaurante=restaurante, menu=menu, fecha=fecha_menu)
 
 
-if __name__ == '__main__':
-    init_db()
-    app.run(debug=True)
+if __name__ == "__main__":
+    ##app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=False)
